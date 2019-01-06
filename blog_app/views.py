62c12edc5dd39a7ac3be1a72e8dffedb8f0af170
LilChou <pcsvl97249@gmail.com>
2019-01-06 15:38:18 -0500

    dynamic blog (sort by publish date)

--- blog_app/views.py
+++ blog_app/views.py
@@ -2,5 +2,29 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+import datetime
+# from polls.model import Poll
+from django.utils import timezone
+from .models import Post
+
+
 def post_list(request):
-	return render(request, 'blog_app/post_list.html', {})
+	# return render(request, 'blog_app/post_list.html', {})
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+	return render(request, 'blog_app/post_list.html', {'posts':posts})
+
+def current_datetime(request):
+	now = datetime.datetime.now()
+	html = "<html><body> It is now %s. </body></html>" % now
+	return HttpResponse(html)
+
+# def detail(request, poll_id):
+# 	try:
+# 		p = Poll.object.get(pk=poll_id)
+# 	except Poll.DoesNotExist:
+# 		raise Http404("Poll does not exist")
+
+# 	return render(request, 'polls/detail.html', {'poll':p})
+
+	
